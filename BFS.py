9fe570e8a4e000c65155ee34b5fe41d722465083
Noway-tech-cpu <devon590111@gmail.com>
2026-03-17 09:27:31 +0530

    first commit

---
+++ BFS.py
@@ -0,0 +1,29 @@
+from collections import deque
+
+def bfs(graph, start):
+    visited = set()          # To keep track of visited nodes
+    queue = deque([start])   # Queue for BFS
+
+    visited.add(start)
+
+    while queue:
+        node = queue.popleft()
+        print(node, end=" ")
+
+        for neighbor in graph[node]:
+            if neighbor not in visited:
+                visited.add(neighbor)
+                queue.append(neighbor)
+
+# Example graph (adjacency list)
+graph = {
+    'A': ['B', 'C'],
+    'B': ['D', 'E'],
+    'C': ['F'],
+    'D': [],
+    'E': ['F'],
+    'F': []
+}
+
+print("BFS Traversal starting from A:")
+bfs(graph, 'A')
